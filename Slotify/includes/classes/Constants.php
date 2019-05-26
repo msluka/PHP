648d60966398882d1434ccd407401454d0ffc6b3
Msluka <mamulashvilis@gmail.com>
2019-05-26 13:29:17 +0200

    Login & Registration

---
+++ Slotify/includes/classes/Constants.php
@@ -0,0 +1,22 @@
+<?php
+
+    class Constants{
+
+        public static $userNameCharacters = "Your username must be between 5 nad 25 characters";
+        public static $firstNameCharacters = "Your firstname must be between 2 nad 25 characters";
+        public static $lastNameCharacters = "Your lastname must be between 2 nad 25 characters";
+        public static $emailInvalide = "Email is invalid";
+        public static $emailsDoNotMatch = "Your emails don't match";       
+        public static $passwordsDoNotMatch = "Your passwords don't match";
+        public static $passwordNotAlphanumeric = "Your passwords can only contain letters and numbers";
+        public static $passwordCharacters = "Your password must be between 5 nad 30 characters";
+        public static $usernameTaken = "This username already exists";
+        public static $emailTaken = "This email already exists";
+
+        public static $loginFaild = "Your username or password was incorrect";
+        
+       
+
+    }
+
+?>
---
+++ Slotify/index.php
@@ -0,0 +1,50 @@
+<?php
+
+    include("includes/config.php");
+
+    if(isset($_SESSION['userLoggedIn'])){
+
+        $userLogggedIn = $_SESSION['userLoggedIn'];
+
+    }else{
+
+        header("Location: register.php");
+
+    }
+
+    
+
+
+?>
+
+<!doctype html>
+
+<head></head>
+
+<body>
+
+    <?php
+        
+        $t = date("H:m:s Y-m-t");
+
+        if ($t < "20") {
+            echo "Have a good day! ".$t;
+        } else {
+            echo "Have a good night! ".$t;
+        }
+
+    ?>
+
+
+    <div>This is me</div>
+    <table>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+
+</body>
+
+</html>
